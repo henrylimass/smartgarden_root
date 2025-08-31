0a7f1422c1fc68c18b048aa55ccab1c14d018f93
HENRY ARAUJO <112120971+henrylimass@users.noreply.github.com>
2025-08-31 18:55:19 -0400

    INICIO
    Ambiente arrumado

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Main extends StatelessWidget{
+@override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+
+
+}
